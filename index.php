5b0b13ad9adfd3f38ab26029b92fe56c9095e6ec
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-07-18 03:52:32 +0200

    Fixed redirect script.

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
-// Which version should we use?
-$version = '2';
-
-header('Location: https://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . 'v' . $version);
+	header('Location: v2/index.php');
 ?>
