e708066ebfde6be1bfa6082740f670c55677da1b
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-07-27 02:09:18 +0200

    Added authenticated check for crew listing.

--- v2/pages/crew.php
+++ v2/pages/crew.php
@@ -1,23 +1,28 @@
 <?php
+require_once 'session.php';
 require_once 'handlers/grouphandler.php';
 require_once 'handlers/teamhandler.php';
 
 $groupId = isset($_GET['id']) ? $_GET['id'] : 0;
 $teamId = isset($_GET['teamId']) ? $_GET['teamId'] : 0;
 
-if (isset($_GET['id'])) {
-	if (isset($_GET['teamId'])) {
-		$team = TeamHandler::getTeam($teamId);
-		$team->displayWithInfo();
+if (Session::isAutenticated()) {
+	if (isset($_GET['id'])) {
+		if (isset($_GET['teamId'])) {
+			$team = TeamHandler::getTeam($teamId);
+			$team->displayWithInfo();
+		} else {
+			$group = GroupHandler::getGroup($groupId);
+			$group->displayWithInfo();
+		}
 	} else {
-		$group = GroupHandler::getGroup($groupId);
-		$group->displayWithInfo();
+		$groupList = GroupHandler::getGroups();
+		
+		foreach ($groupList as $group) {	
+			$group->displayWithInfo();
+		}
 	}
 } else {
-	$groupList = GroupHandler::getGroups();
-	
-	foreach ($groupList as $group) {	
-		$group->displayWithInfo();
-	}
+	echo '<p>Du er ikke logget inn!</p>';
 }
 ?>
