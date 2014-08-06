925fb2151e47c92789f582ba3bd9ce313d4d7fcd
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-08-07 01:16:48 +0200

    Work on error handler

---
+++ v2/scripts/sharedScripts.js
@@ -0,0 +1,12 @@
+function error(what)
+{
+	//Do something
+	$("#error").html(what);
+	$("#error").fadeIn(300);
+}
+function info(what)
+{
+	//Do even more something
+	$("#info").html(what);
+	$("#info"),fadeIn(300);
+}
