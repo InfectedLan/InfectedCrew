a9cf88a0be3bdb94431b61c06590b0f065a68ebf
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-09-05 18:50:16 +0200

    Added google analytics code

--- v2/site.php
+++ v2/site.php
@@ -36,6 +36,15 @@ class Site {
 				echo '<script src="../api/scripts/login.js"></script>';
 				echo '<script src="../api/scripts/logout.js"></script>';
 				echo '<script src="scripts/common.js"></script>';
+				echo '<script>';
+					echo '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){';
+					echo '(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),';
+					echo 'm=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)';
+					echo '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');';
+
+					echo 'ga(\'create\', \'UA-54254513-3\', \'auto\');';
+					echo 'ga(\'send\', \'pageview\');';
+				echo '</script>';	
 			echo '</head>';
 			echo '<body>';
 				echo '<header>';
