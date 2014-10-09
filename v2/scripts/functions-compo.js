40db3e7f4e49483b9ddd24d29133cf0f812732b5
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-10-09 22:39:08 +0200

    Added framework thingie

--- v2/scripts/functions-compo.js
+++ v2/scripts/functions-compo.js
@@ -1,1 +1,23 @@
-//TODO
+$(document).ready(function() {
+	setInverval(renderMatchList, 5000);
+});
+function renderMatchList() {
+	$.getJSON('../api/json/getMatchList.php' + '?id=' + compoId, function(data) {
+		if (data.result) {
+			renderPendingMatches(data.data.pending);
+			renderCurrentMatches(data.data.current);
+			renderFinishedMatches(data.data.finished);
+		} else {
+			error(data.message); 
+		}
+	});
+}
+function renderPendingMatches(pendingData) {
+
+}
+function renderCurrentMatches(currentData) {
+
+}
+function renderFinishedMatches(finishedData) {
+	
+}
