c24e3525efa94d1bd53bb6b58c72bc3755ae9aee
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-10-12 14:16:15 +0200

    Recoded layout for compo list

--- v2/scripts/functions-compo.js
+++ v2/scripts/functions-compo.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	setInterval(renderMatchList, 5000);
+	renderMatchList();
 });
 function renderMatchList() {
 	$.getJSON('../api/json/getMatchList.php' + '?id=' + compoId, function(data) {
@@ -17,28 +18,22 @@ function renderPendingMatches(pendingData) {
 	var appendArray = [];
 	appendArray.push("<h1>Ventende matcher</h1>");
 	
+	appendArray.push('<hr />');
 	for(var i = 0; i < pendingData.length; i++) {
 		appendArray.push('<h3>Match id: ' + pendingData[i].id + '</h3>');
+		appendArray.push('<b>Start: </b>' + pendingData[i].startString + ' (' + pendingData[i].startTime + ')<br />');
+		appendArray.push('<b>Connect data: </b>' + pendingData[i].connectData + '<br />');
+		appendArray.push('<b>Participants: </b><br />');
 		appendArray.push("<table>");
-			appendArray.push('<tr>');
-				appendArray.push('<td>');
-					appendArray.push(pendingData[i].startTime);
-				appendArray.push('</td>');
-				appendArray.push('<td>');
-					appendArray.push(pendingData[i].startString);
-				appendArray.push('</td>');
-				appendArray.push('<td>');
-					appendArray.push(pendingData[i].connectData);
-				appendArray.push('</td>');
-			appendArray.push("</tr>");
 			for(var x = 0; x < pendingData[i].participants.length; x++) {
 				appendArray.push('<tr>');
-					appendArray.push('<td colspan="2">');
+					appendArray.push('<td>');
 						appendArray.push(pendingData[i].participants[x]);
 					appendArray.push('</td>');
 				appendArray.push('</tr>');
 			}
 		appendArray.push("</table>");
+		appendArray.push('<hr />');
 	}
 	
 	$("#teamListArea").append(appendArray.join(""));
