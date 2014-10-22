5fa4b6a88bd34bd2d3424d029466c688f38a9fb0
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-10-22 22:44:23 +0200

    Added sending of commands n shit

--- v2/scripts/admin-api.js
+++ v2/scripts/admin-api.js
@@ -9,6 +9,9 @@ $(document).ready(function(){
 		$("#apiName").append(o);
 	}
 	showCommandParameters(0);
+	$('#apiName').change(function() {
+		showCommandParameters($('#apiName')[0].selectedIndex);
+	});
 });
 function showCommandParameters(index) {
 	var command = apiCommands[index];
@@ -33,5 +36,20 @@ function showCommandParameters(index) {
 	$("#commandArgBox").html(commandArgsText.join(""));
 }
 function submitQuery() {
+	var commandIndex = $('#apiName')[0].selectedIndex;
+	var queryString = '../api/json/';
+	console.log("Command index: " + commandIndex);
 
+	queryString = queryString + apiCommands[commandIndex].name;
+	queryString = queryString + '.php';
+	for(var i = 0; i < apiCommands[commandIndex].args.length; i++) {
+		queryString = queryString + (i == 0 ? '?' : '&') + apiCommands[commandIndex].args[i].name + '=' + $('#commandParameter' + i).val();
+	}
+	$.getJSON(queryString, function(data) {
+		if (data.result) {
+			$("#").html(JSON.stringify(data, undefined, 4));
+		} else {
+			error(data.message); 
+		}
+	});
 }
