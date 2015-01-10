8ec172b5d5866b336f69b01ebedb1849009b2e38
Liam Svanåsbakken Crouch <petterroea@skymiastudios.com>
2015-01-10 16:35:53 +0100

    Allow local development without using hours setting up ssl

--- v2/index.php
+++ v2/index.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'site.php';
 
-// Force user to use https.
-if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
+// Force user to use https if we are not in a development environment. Seriously, i wont want to spend 1 hour setting up my dev environment because one line cannot allow me non-ssl.
+if ( ( !isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on' ) && strpos($_SERVER['HTTP_HOST'], "localhost") == -1 ) {
 	header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
 }
 
