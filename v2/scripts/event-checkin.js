ddd5788872dcb05982625a83b4c2e747e82ed96d
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-02-11 22:48:38 +0100

    Improved event-checkin.js

--- v2/scripts/event-checkin.js
+++ v2/scripts/event-checkin.js
@@ -1,24 +1,66 @@
-var passThroughId = 0;
 function loadData() {
 	var ticketId = $("#ticketId").val();
-	passThroughId = ticketId;
-	$.getJSON('../api/json/ticket/getTicketData.php?id=' + encodeURIComponent(ticketId), function(data){
-		if(data.result) {
-			$("#ticketDetails").append("<table>" +
-				"<tr><td>Fullt navn</td><td>" + data.userData.fullName + "</td></tr>" +
-				"<tr><td>Kjønn</td><td>" + data.userData.gender + "</td></tr>" +
-				"<tr><td>Født</td><td>" + data.userData.birthDate + "</td></tr>" +
-				"<tr><td>Alder</td><td>" + data.userData.age + "</td></tr>" +
-				"</table><input type='button' value='Godkjenn' onClick='acceptTicket(" + passThroughId + ")' />");
+
+	$.getJSON('../api/json/ticket/getTicketData.php?id=' + encodeURIComponent(ticketId), function(data) {
+		if (data.result) {
+			// Remove old entries.
+			$("#ticketDetails").empty();
+			var user = data.userData[0];
+
+			$("#ticketDetails").append('<table>' +
+									   	   '<tr>' +
+										       '<td>Navn:</td>' +
+										       '<td>' + user.firstname + ' ' + user.lastname +'</td>' +
+										   '</tr>' +
+										   '<tr>' +
+										   	   '<td>Adresse:</td>' +
+										   	   '<td>' + user.address + '</td>' +
+										   '</tr>' +
+										   '<tr>' +
+										   	   '<td></td>' +
+										   	   '<td>' + user.city + '</td>' +
+										   '</tr>' +
+										   '<tr>' +
+										   	   '<td>Kjønn:</td>' +
+										   	   '<td>' + user.gender + '</td>' +
+										   '</tr>' +
+										   '<tr>' +
+										   	   '<td>Født:</td>' +
+										       '<td>' + user.birthdate + '</td>' +
+										   '</tr>' +
+										   '<tr>' +
+										       '<td>Alder:</td>' +
+										   	   '<td>' + user.age + ' År</td>' +
+										   '</tr>' +
+										   '<tr>' +
+										   	   '<td>Brukernavn:</td>' +
+										   	   '<td>' + user.username + '</td>' +
+										   '</tr>' +
+										   '<tr>' +
+										   	   '<td>E-post:</td>' +
+										   	   '<td>' + user.email + '</td>' +
+										   '</tr>' +
+										   '<tr>' +
+										   	   '<td>Phone:</td>' +
+										   	   '<td>' + user.phone + '</td>' +
+										   '</tr>' +
+
+									   '</table>' +
+									   '<input type="button" value="Godkjenn" onClick="acceptTicket(' + ticketId + ')">');
 		} else {
 			error(data.message);
 		}
 	});
 }
+
 function acceptTicket(id) {
-	$.getJSON('../api/json/ticket/checkInTicket.php?id=' + encodeURIComponent(id), function(data){
-		if(data.result) {
-			location.reload();
+	$.getJSON('../api/json/ticket/checkInTicket.php?id=' + encodeURIComponent(id), function(data) {
+		if (data.result) {
+			// Remove the user information.
+			$("#ticketDetails").empty();
+
+			// Display confirmation message to the user.
+			info(data.message);			
 		} else {
 			error(data.message);
 		}
