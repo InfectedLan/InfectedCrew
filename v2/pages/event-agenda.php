f92736a03f029674bd4e9d34b31fc40c0dc22f7e
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-08-16 16:56:44 +0200

    Made it possible to access these pages without being a member if the user has the needed permissions.

--- v2/pages/event-agenda.php
+++ v2/pages/event-agenda.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -23,84 +23,78 @@ require_once 'handlers/agendahandler.php';
 
 if (Session::isAuthenticated()) {
 	$user = Session::getCurrentUser();
-	
-	if ($user->isGroupMember()) {
-		$group = $user->getGroup();
-		
-		if ($user->hasPermission('*') || 
-			$user->hasPermission('event.agenda')) {
-			echo '<script src="scripts/event-agenda.js"></script>';
-			echo '<h3>Agenda</h3>';
-			
-			$agendaList = AgendaHandler::getAgendas();
-			
-			if (!empty($agendaList)) {
-				echo '<table>';
+
+	if ($user->hasPermission('*') ||
+		$user->hasPermission('event.agenda')) {
+		echo '<script src="scripts/event-agenda.js"></script>';
+		echo '<h3>Agenda</h3>';
+
+		$agendaList = AgendaHandler::getAgendas();
+
+		if (!empty($agendaList)) {
+			echo '<table>';
+				echo '<tr>';
+					echo '<th>Navn:</th>';
+					echo '<th>Informasjon:</th>';
+					echo '<th>Tid:</th>';
+				echo '</tr>';
+
+				foreach ($agendaList as $agenda) {
 					echo '<tr>';
-						echo '<th>Navn:</th>';
-						echo '<th>Informasjon:</th>';
-						echo '<th>Tid:</th>';
+						echo '<form class="agenda-edit" method="post">';
+							echo '<input type="hidden" name="id" value="' . $agenda->getId() . '">';
+							echo '<td><input type="text" name="title" value="' . $agenda->getTitle() . '"></td>';
+							echo '<td><textarea name="description">' . $agenda->getDescription() . '</textarea></td>';
+							echo '<td>';
+								echo '<input type="time" name="startTime" value="' . date('H:i', $agenda->getStartTime()) . '">';
+								echo '<br>';
+								echo '<input type="date" name="startDate" value="' . date('Y-m-d', $agenda->getStartTime()) . '">';
+							echo '</td>';
+
+							if ($agenda->isPublished()) {
+								echo '<td><input type="checkbox" name="published" value="1" checked></td>';
+							} else {
+								echo '<td><input type="checkbox" name="published" value="1"></td>';
+							}
+
+							echo '<td><input type="submit" value="Endre"></td>';
+						echo '</form>';
+						echo '<td><input type="button" value="Fjern" onClick="removeAgenda(' . $agenda->getId() . ')"></td>';
 					echo '</tr>';
-					
-					foreach ($agendaList as $agenda) {
-						echo '<tr>';
-							echo '<form class="agenda-edit" method="post">';
-								echo '<input type="hidden" name="id" value="' . $agenda->getId() . '">';
-								echo '<td><input type="text" name="title" value="' . $agenda->getTitle() . '"></td>';
-								echo '<td><textarea name="description">' . $agenda->getDescription() . '</textarea></td>';
-								echo '<td>';
-									echo '<input type="time" name="startTime" value="' . date('H:i', $agenda->getStartTime()) . '">';
-									echo '<br>';
-									echo '<input type="date" name="startDate" value="' . date('Y-m-d', $agenda->getStartTime()) . '">';
-								echo '</td>';
-								
-								if ($agenda->isPublished()) {
-									echo '<td><input type="checkbox" name="published" value="1" checked></td>';
-								} else {
-									echo '<td><input type="checkbox" name="published" value="1"></td>';
-								}
-								
-								echo '<td><input type="submit" value="Endre"></td>';
-							echo '</form>';
-							echo '<td><input type="button" value="Fjern" onClick="removeAgenda(' . $agenda->getId() . ')"></td>';
-						echo '</tr>';
-					}
-				echo '</table>';
-			} else {
-				echo '<p>Det er ikke opprettet noen agenda\'er enda.';
-			}
-			
-			echo '<h3>Legg til ny agenda:</h3>';
-			echo '<p>Fyll ut feltene under for å legge til en ny agenda.</p>';
-			
-			echo '<form class="agenda-add" method="post">';
-				echo '<table>';
-					echo '<tr>';
-						echo '<td>Navn:</td>';
-						echo '<td><input type="text" name="title"></td>';
-					echo '</tr>';
-					echo '<tr>';
-						echo '<td>Informasjon:</td>';
-						echo '<td><textarea class="editor" name="description"></textarea></td>';
-					echo '</tr>';
-					echo '<tr>';
-						echo '<td>Tid:</td>';
-						echo '<td><input type="time" name="startTime" value="' . date('H:i:s') . '"></td>';
-					echo '</tr>';
-					echo '<tr>';
-						echo '<td>Dato:</td>';
-						echo '<td><input type="date" name="startDate" value="' . date('Y-m-d') . '"></td>';
-					echo '</tr>';
-					echo '<tr>';
-						echo '<td><input type="submit" value="Legg til"></td>';
-					echo '</tr>';
-				echo '</table>';
-			echo '</form>';
+				}
+			echo '</table>';
 		} else {
-			echo '<p>Du har ikke rettigheter til dette!</p>';
+			echo '<p>Det er ikke opprettet noen agenda\'er enda.';
 		}
+
+		echo '<h3>Legg til ny agenda:</h3>';
+		echo '<p>Fyll ut feltene under for å legge til en ny agenda.</p>';
+
+		echo '<form class="agenda-add" method="post">';
+			echo '<table>';
+				echo '<tr>';
+					echo '<td>Navn:</td>';
+					echo '<td><input type="text" name="title"></td>';
+				echo '</tr>';
+				echo '<tr>';
+					echo '<td>Informasjon:</td>';
+					echo '<td><textarea class="editor" name="description"></textarea></td>';
+				echo '</tr>';
+				echo '<tr>';
+					echo '<td>Tid:</td>';
+					echo '<td><input type="time" name="startTime" value="' . date('H:i:s') . '"></td>';
+				echo '</tr>';
+				echo '<tr>';
+					echo '<td>Dato:</td>';
+					echo '<td><input type="date" name="startDate" value="' . date('Y-m-d') . '"></td>';
+				echo '</tr>';
+				echo '<tr>';
+					echo '<td><input type="submit" value="Legg til"></td>';
+				echo '</tr>';
+			echo '</table>';
+		echo '</form>';
 	} else {
-		echo '<p>Du er ikke medlem av en gruppe.</p>';
+		echo '<p>Du har ikke rettigheter til dette!</p>';
 	}
 } else {
 	echo '<p>Du er ikke logget inn!</p>';
--- v2/pages/event-checkin.php
+++ v2/pages/event-checkin.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,7 +24,7 @@ require_once 'handlers/eventhandler.php';
 
 if (Session::isAuthenticated()) {
 	$user = Session::getCurrentUser();
-	
+
 	if ($user->hasPermission('*') ||
 		$user->hasPermission('event.checkin')) {
 		echo '<script src="scripts/event-checkin.js"></script>';
@@ -32,9 +32,9 @@ if (Session::isAuthenticated()) {
 
 		$event = EventHandler::getCurrentEvent();
 		$season = date('m', $event->getStartTime()) == 2 ? 'VINTER' : 'HØST';
-		$eventName = !empty($event->getTheme()) ? $event->getTheme() : $season . date('Y', $event->getStartTime());
-		
-		echo Settings::name . '_' . $eventName . '_' . '<input id="ticketId" type="text" autofocus>';
+		$eventName = $season . '_' . date('Y', $event->getStartTime());
+
+		echo strtoupper(Settings::name . '_' . $eventName . '_') . '<input id="ticketId" type="text" autofocus>';
 		echo '<br>';
 		echo '<input type="button" value="Sjekk inn" onClick="loadData()"/>';
 		echo '<br>';
