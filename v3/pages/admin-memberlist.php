3a136013861ef0f62007a3ad102e788274344b71
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-09-29 15:21:37 +0200

    Moved memberlist to admin.

--- v3/pages/admin-memberlist.php
+++ v3/pages/admin-memberlist.php
@@ -17,69 +17,89 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
+ require_once 'admin.php';
+ require_once 'session.php';
+ require_once 'handlers/eventhandler.php';
+ require_once 'interfaces/page.php';
 
-require_once 'session.php';
-require_once 'handlers/eventhandler.php';
+class AdminMemberListPage extends AdminPage implements IPage {
+ 	public function getTitle() {
+ 		return 'Medlemsliste';
+ 	}
 
-if (Session::isAuthenticated()) {
-	$user = Session::getCurrentUser();
+ 	public function getContent() {
+ 		$content = null;
 
-	if ($user->hasPermission('admin.memberlist')) {
-		echo '<script src="scripts/event-memberlist.js"></script>';
-		echo '<h3>Medlemsliste</h3>';
+ 		if (Session::isAuthenticated()) {
+ 			$user = Session::getCurrentUser();
 
-		echo '<p>Velg år du vil hente ut medlemsliste for, maksimal alder på medlemmene du vil ha med og et format du vil ha listen i.<br></p>';
+ 			if ($user->hasPermission('admin.memberlist')) {
+				$content .= '<script src="scripts/admin-memberlist.js"></script>';
 
-		echo '<form class="memberlist" method="post">';
-			echo '<table>';
-				echo '<tr>';
-					echo '<td>År:</td>';
-					echo '<td>';
-						echo '<select name="year">';
-							$eventList = EventHandler::getEvents();
+				$content .= '<div class="row">';
+					$content .= '<div class="col-md-6">';
+						$content .= '<div class="box">';
+							$content .= '<div class="box-body">';
+								$content .= '<p>Velg år du vil hente ut medlemsliste for, maksimal alder på medlemmene du vil ha med og et format du vil ha listen i.</p>';
+								$content .= '<form class="memberlist" method="post">';
+									$content .= '<div class="form-group">';
+										$content .= '<label>År</label>';
+										$content .= '<select class="form-control" name="year">';
+											$eventList = EventHandler::getEvents();
 
-							for ($year = date('Y', reset($eventList)->getStartTime()); $year <= date('Y'); $year++) {
-								if ($year == date('Y')) {
-									echo '<option value="' . $year . '" selected>' . $year . '</option>';
-								} else {
-									echo '<option value="' . $year . '">' . $year . '</option>';
-								}
-							}
-						echo '</select>';
-					echo '</td>';
-				echo '</tr>';
-				echo '<tr>';
-					echo '<td>Aldersgrense:</td>';
-					echo '<td>';
-						echo '<select name="ageLimit">';
-							for ($age = 1; $age <= 100; $age++) {
-								if ($age == 20) {
-									echo '<option value="' . $age . '" selected>' . $age . '</option>';
-								} else {
-									echo '<option value="' . $age . '">' . $age . '</option>';
-								}
-							}
-						echo '</select> År';
-					echo '</td>';
-				echo '</tr>';
-				echo '<tr>';
-					echo '<td>Format:</td>';
-					echo '<td>';
-						echo '<select name="format">';
-							echo '<option value="html" selected>Tekst</option>';
-							echo '<option value="csv">Regneark</option>';
-						echo '</select>';
-					echo '</td>';
-				echo '</tr>';
-				echo '<tr>';
-					echo '<td><input type="submit" value="Hent"></td>';
-				echo '</tr>';
-			echo '</table>';
-		echo '</form>';
-	} else {
-		echo '<p>Du har ikke rettigheter til dette!</p>';
+											for ($year = date('Y', reset($eventList)->getStartTime()); $year <= date('Y'); $year++) {
+												if ($year == date('Y')) {
+													$content .= '<option value="' . $year . '" selected>' . $year . '</option>';
+												} else {
+													$content .= '<option value="' . $year . '">' . $year . '</option>';
+												}
+											}
+
+										$content .= '</select>';
+									$content .= '</div>';
+									$content .= '<div class="form-group">';
+										$content .= '<label>Aldersgrense</label>';
+										$content .= '<select class="form-control" name="ageLimit">';
+
+											for ($age = 1; $age <= 100; $age++) {
+												if ($age == 20) {
+													$content .= '<option value="' . $age . '" selected>' . $age . ' År</option>';
+												} else {
+													$content .= '<option value="' . $age . '">' . $age . ' År</option>';
+												}
+											}
+
+										$content .= '</select>';
+									$content .= '</div>';
+									$content .= '<div class="form-group">';
+										$content .= '<label>Format</label>';
+										$content .= '<select class="form-control" name="format">';
+											$content .= '<option value="html" selected>Tekst (.html)</option>';
+											$content .= '<option value="csv">Regneark (.csv)</option>';
+										$content .= '</select>';
+									$content .= '</div><!-- /.form group -->';
+									$content .= '<button type="submit" class="btn btn-primary">Hent</button>';
+								$content .= '</form>';
+							$content .= '</div><!-- /.box-body -->';
+						$content .= '</div><!-- /.box -->';
+					$content .= '</div><!--/.col (left) -->';
+				$content .= '</div><!-- /.row -->';
+			} else {
+				$content .= '<div class="box">';
+					$content .= '<div class="box-body">';
+						$content .= '<p>Du har ikke rettigheter til dette.</p>';
+					$content .= '</div><!-- /.box-body -->';
+				$content .= '</div><!-- /.box -->';
+			}
+		} else {
+			$content .= '<div class="box">';
+				$content .= '<div class="box-body">';
+					$content .= '<p>Du er ikke logget inn.</p>';
+				$content .= '</div><!-- /.box-body -->';
+			$content .= '</div><!-- /.box -->';
+		}
+
+		return $content;
 	}
-} else {
-	echo '<p>Du er ikke logget inn!</p>';
 }
 ?>
